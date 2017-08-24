cf69f8262d1f383bcd27466bee5f7e78fba7b9d6
J_sky <bosichong@qq.com>
2017-08-24 23:09:36 +0800

    修改了404添加了腾讯公益

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9@47ifi$ren*wzm&c7%^oa-gg9py_&i77pgtbn8t)1qf8_yl09'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
